10c1ed2c7a718af77993361a39fbac0e292d7a79
Marc Immediato <marc.immediato@gmail.com>
2018-12-13 19:02:40 -0500

    hooking up server to prisma

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: (root, args, context, info) => {
-      return context.db.query.links({}, info)
-    },
+      return context.db.query.links({}, info);
+    }
   },
   Mutation: {
     post: (root, args, context, info) => {
-      return context.db.mutation.createLink({
-        data: {
-          url: args.url,
-          description: args.description,
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
         },
-      }, info)
-    },
-  },
-}
+        info
+      );
+    }
+  }
+};
 
 // 3
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/marc-immediato-803f33/database/dev',
+      secret: 'adggedgabadsbbbdedi39ff848d',
+      debug: true
+    })
+  })
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
